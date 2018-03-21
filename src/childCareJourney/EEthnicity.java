c3665beab840cbd97d60e209d7c11213a30d8713
Tim Martin <tpmartin7@gmail.com>
2018-03-21 19:18:33 -0400

    Updated enums for referrals

--- src/childCareJourney/EEthnicity.java
+++ src/childCareJourney/EEthnicity.java
@@ -1,5 +1,11 @@
 package childCareJourney;
 
 public enum EEthnicity {
-
+	WHITE,
+	BLACK,
+	ASIAN,
+	ORIENTAL,
+	MIXED,
+	OTHER,
+	UNKOWN
 }
--- src/childCareJourney/Referral.java
+++ src/childCareJourney/Referral.java
@@ -1,5 +1,46 @@
 package childCareJourney;
 
-public class Referral {
+import java.util.Date;
 
+public class Referral {
+	
+	private Integer id;
+	private Integer ageAtReferral;
+	private EGender gender;
+	private EEthnicity ethnicity;
+	private EWard ward;
+	private Date referralDate;
+
+	public Referral(Integer id, Integer age, EGender gender, EEthnicity ethnicity, EWard ward, Date date) {
+		this.id = id;
+		this.ageAtReferral = age;
+		this.gender = gender;
+		this.ethnicity = ethnicity;
+		this.ward = ward;
+		this.referralDate = date;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getAgeAtReferral() {
+		return ageAtReferral;
+	}
+
+	public EGender getGender() {
+		return gender;
+	}
+
+	public EEthnicity getEthnicity() {
+		return ethnicity;
+	}
+
+	public EWard getWard() {
+		return ward;
+	}
+
+	public Date getReferralDate() {
+		return referralDate;
+	}
 }
